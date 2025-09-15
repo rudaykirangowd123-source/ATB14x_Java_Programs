cf8ed44fae6159fe68e5b5b152dc46278f869873
ATB14x_Java_Programs <rudaykirangowd123@gmail.com>
2025-09-15 22:27:14 +0530

    Table9

---
+++ src/Lab_01_Table9.java
@@ -0,0 +1,16 @@
+public class Lab_01_Table9 {
+    public static void main(String[] args) {
+        int table = 9;
+        char newline= '\n';
+        System.out.printf("%dx1=%d"+newline, table, table);
+        System.out.printf("%dx2=%d"+newline, table, table*2);
+        System.out.printf("%dx3=%d"+newline, table, table*3);
+        System.out.printf("%dx4=%d"+newline, table, table*4);
+        System.out.printf("%dx5=%d"+newline, table, table*5);
+        System.out.printf("%dx6=%d"+newline, table, table*6);
+        System.out.printf("%dx7=%d"+newline, table, table*7);
+        System.out.printf("%dx8=%d"+newline, table, table*8);
+        System.out.printf("%dx9=%d"+newline, table, table*9);
+        System.out.printf("%dx10=%d"+newline, table, table*10);
+    }
+}
