092379b17263ce5d2a663605b11d24972f7c982f
ATB14x_Java_Programs <rudaykirangowd123@gmail.com>
2025-09-24 19:43:56 +0530

    Ternary operator

---
+++ src/ex_03_Ternary_operator/Lab15_switch.java
@@ -0,0 +1,26 @@
+package ex_03_Ternary_operator;
+
+public class Lab15_switch {
+    public static void main(String[] args) {
+        int i=2;
+        switch(i)
+
+        {
+
+            case 1:
+
+            case 2:
+                System.out.println("tuesday");
+
+            case 3:
+                System.out.println("tuesday");
+
+                break;
+
+            case 5:
+
+                break;
+
+        }
+    }
+}
